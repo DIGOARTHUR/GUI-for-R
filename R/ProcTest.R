62bfc5ff3d1e46479697cc5fc73dae994463dee0
digoarthur <digo.arthur@hotmail.com>
2024-01-01 11:25:42 +0000

    refactor: just a GUI file

--- R/ProcTest.R
+++
@@ -1,8 +0,0 @@
-# Internal function of MRtest
-ProcTest <- function(Teste) {
-  if (is.matrix(Teste) == FALSE) {
-    g  <- diag(length(Teste))
-    Teste <- cbind(Teste, g)
-  }
-  return(Teste)
-}
